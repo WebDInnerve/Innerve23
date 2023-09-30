555afe2c2e93bd391c327a5af3c50fed9a31c05e
Aastha Singhal <aastha.tanya21@gmail.com>
2023-09-30 12:26:19 +0530

    all pages added

--- Events Page/ECE Page/footer.js
+++
@@ -1,9 +0,0 @@
-// add extra elements to buttons for hover, keeps html cleaner
-$('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
-
-$('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
-
-const linkedln = document.getElementByI('linkedln')
-
-
-linkedln.style.backgroundImage = "url(https://static.vecteezy.com/system/resources/previews/018/930/587/original/linkedin-logo-linkedin-icon-transparent-free-png.png)"
